375af93898172ea5e2bc392feb9760ce8644ede8
Monika <monikasembiring34@yahoo.com>
2014-10-14 09:10:00 +0700

    Upload Tugas 1

--- parsingrss.py
+++ parsingrss.py
@@ -1,14 +1,42 @@
 from xml.dom import minidom
+import time
 import urllib
+import sys
 
-url_str = 'http://feeds.bbci.co.uk/news/rss.xml?edition=int'
-xml_str = urllib.urlopen(url_str).read()
-xmldoc = minidom.parseString(xml_str)
+if len(sys.argv) > 1:
+	xmlurl = sys.argv[1]
+	print 'Retrieving RSS feed from ' + xmlurl
+	if xmlurl != '':
+		testfile = urllib.URLopener()
+		testfile.retrieve(xmlurl, "bbcrss.xml")
 
-item_values = xmldoc.getElementsByTagName('item')
+xmldoc = minidom.parse('bbcrss.xml')
 
-for s in item_values:
-	print s.childNodes[1].nodeName + " = " + s.childNodes[1].childNodes[0].nodeValue
-	print s.childNodes[3].nodeName + " = " + s.childNodes[3].childNodes[0].nodeValue	
-	print s.childNodes[9].nodeName + " = " + s.childNodes[9].childNodes[0].nodeValue
-	
+mychannel = xmldoc.getElementsByTagName('channel')[0]
+rsstitle = mychannel.getElementsByTagName('title')[0]
+rssdesc = mychannel.getElementsByTagName('description')[0]
+
+print ''
+print rsstitle.childNodes[0].nodeValue + ' | ' + rssdesc.childNodes[0].nodeValue
+print ''
+print '====================================================='
+rssupdated = 'Updated on ' + mychannel.getElementsByTagName('lastBuildDate')[0].childNodes[0].nodeValue
+print rssupdated
+print '====================================================='
+
+itemlist = mychannel.getElementsByTagName('item')
+itemlist.sort(key=lambda x: time.strptime(x.getElementsByTagName('pubDate')[0].childNodes[0].nodeValue, '%a, %d %b %Y %H:%M:%S %Z'), reverse=True)
+
+i = 1
+for s in itemlist :
+	if i<=52 :
+	#if s.getElementsByTagName('pubDate')[0].childNodes[0].nodeValue <= 'Tue, 14 Oct 2014 00:17:56 GMT' and s.getElementsByTagName('pubDate')[0].childNodes[0].nodeValue >= 'Mon, 13 Oct 2014 00:17:56 GMT' :
+		itemtitle = s.getElementsByTagName('title')[0]
+		print str(i) + '. ' + itemtitle.childNodes[0].nodeValue
+		i = i+1 
+		pubDate = s.getElementsByTagName('pubDate')[0]
+		print '    Published on ' + pubDate.childNodes[0].nodeValue
+		itemcontent = s.getElementsByTagName('description')[0]
+		itemcontent = s.getElementsByTagName('guid')[0]
+		print '    Link : ' + itemcontent.childNodes[0].nodeValue
+		print ''
